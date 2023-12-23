9e27589fa52dc644378f684d312fe32979f0fd8a
Junaid <junaidhayat33303@gmail.com>
2023-12-23 23:58:09 +0500

    another commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from "react-icons/fa";
+import svg from './svg'
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer>
+        <div className="container-fluid bg-dark text-white">
+          <div className="row py-4 footer-upper">
+            <div className="col-lg-5 col-md-12 col-sm-12  ps-5 newsletter-div">
+              <div>
+                <img src="images/newsletter.png" alt="newsletters" />
+                <h2 className="ms-2 me-4 text-white">SignUp For Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-lg-7 col-md-12 col-sm-12 pe-5 input-div">
+              <div>
+                <div class="input-group">
+                  <input type="text" class="form-control  border-0 input-style" placeholder="Your Email" aria-label="Your Email" aria-describedby="button-addon2" />
+                  <button class="btn m-1" type="button" id="button-addon2">SUBSCRIBE</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row footer-lower">
+            <div className="col-lg-3 col-md-3 col-sm-4 col-contact">
+              <h5>contact us</h5>
+              <ul>
+                <li>
+                  <span>
+                    Demo Store <br />
+                    No. 1259 Freedom,<br />
+                    New York, 11111 <br />
+                    United States
+                  </span>
+                </li>
+                <li>
+                  <span>+91-987654321</span>
+                </li>
+                <li><Link to="">demo@example.com</Link></li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-4 col-info">
+              <h5>information</h5>
+              <ul>
+                <li>
+                  <li><Link>Terms of Service</Link></li>
+                  <li><Link>Shipping Policy</Link></li>
+                  <li><Link>Refund Policy</Link></li>
+                  <li><Link>Privacy Policy</Link></li>
+                  <li><Link>Blogs</Link></li>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-4 col-acc">
+              <h5>account</h5>
+              <ul>
+                <li>
+                  <li><Link>Size Chat</Link></li>
+                  <li><Link>About us</Link></li>
+                  <li><Link>Contact</Link></li>
+                  <li><Link>Search</Link></li>
+                  <li><Link>Faq</Link></li>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-12 col-sm-4 col-quickLinks">
+              <h5>quick links</h5>
+              <ul>
+                <li>
+                  <li><Link>Smart Watches</Link></li>
+                  <li><Link>Headphones</Link></li>
+                  <li><Link>Accessories</Link></li>
+                  <li><Link>Laptops</Link></li>
+                  <li><Link>Tablets</Link></li>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6">
+              <h5>our app</h5>
+              <span>Download our App and get extra 15% <br />
+                Discount on your first order..!
+              </span>
+              <div className="row ourApp-row">
+                <div className="col my-2">
+                  <Link to='https://play.google.com/store/games'><img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/app-icon-01.png?v=1654931267" alt="" /></Link>
+                </div>
+                <div className="col">
+                  <Link to='https://www.apple.com/app-store/'> <img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/app-icon-02.png?v=1654931286" alt="" /></Link>
+                </div>
+              </div>
 
-export default Footer
+            </div>
+          </div>
+          <div className="row icon-row">
+            <div className="col py-3">
+              <ul>
+                <li><Link><FaFacebookF /></Link></li>
+                <li><Link><FaTwitter /></Link></li>
+                <li><Link><FaInstagram /></Link></li>
+                <li><Link><FaYoutube /></Link></li>
+                <li><Link><FaPinterest /></Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className="row copyright-row pt-4">
+            <hr />
+            <div className="row d-flex justify-content-between">
+              <div className="col-4 col-sm-6 ps-3 py-4">
+                <p> &copy; {year} - All Right Reserved.</p>
+              </div>
+              <div className="col-4 pay-icon d-flex justify-content-end pe-3 pt-4">
+                {svg.map((props, i) => {
+                  return (
+                    <div className="px-1" key={i}>
+                      {props.image}
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+};
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { BsSearch } from "react-icons/bs"
 const Header = () => {
   return (
@@ -18,7 +18,7 @@ const Header = () => {
       </header>
       <header className="header-upper py-3">
         <div className="container upper-conatiner">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-2">
               <h2>
                 <Link className='text-white'>Vouge</Link>
@@ -26,14 +26,75 @@ const Header = () => {
             </div>
             <div className="col-5">
               <div className="input-group search-input rounded-2">
-                <input type="text" className="form-control" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2" />
-                <span className="input-group-text bg-primary" id="basic-addon2"><BsSearch /></span>
+                <input type="text" className="form-control shadow-none border-none py-2" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2" />
+                <span className="input-group-text bg-primary" id="basic-addon2"><BsSearch className='fs-5' /></span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className='header-upper-links d-flex align-items-center justify-content-between '>
+                <div>
+                  <Link className='d-flex align-items-center gap-6'>
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className='mb-0'>Compare<br />Products</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-6'>
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className='mb-0'>Favourite<br />Wishlist</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-6'>
+                    <img src="images/user.svg" alt="user" />
+                    <p className='mb-0'>Log in<br />My Account</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-6'>
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className='d-flex flex-column gap-1'>
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className='mb-0'>$400</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
+      <header className="header-bottom py-3">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col">
+              <div className='menu-bottom d-flex align-items-center gap-30'>
+                <div>
+                  <div className="dropdown shop-dropdown ">
+                    <button className="btn btn-secondary dropdown-toggle shadow-none bg-transparent border-0 d-flex align-items-center gap-6" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src="images/menu.svg" alt="menu" />
+                      <span className='me-2 d-inline-block'>Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu menu">
+                      <li><Link className="dropdown-item" to="">Action</Link></li>
+                      <li><Link className="dropdown-item" to="">Another action</Link></li>
+                      <li><Link className="dropdown-item" to="">Something else here</Link></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-15'>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/store'>Our Store</NavLink>
+                    <NavLink to='/blogs'>Blogs</NavLink>
+                    <NavLink to='/contact'>Contact</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header >
     </>
   )
 }
